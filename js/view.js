1d1960d91bd60a2619ac9fd0f1a452c6be601c32
Line-r <bussens1231@gmail.com>
2019-02-06 19:37:59 +0100

    DB Fix

--- js/view.js
+++ js/view.js
@@ -1,7 +1,7 @@
 $('document').ready(function(){
   $('body').on("show.bs.collapse", ".qr-collapse", function(event){
     var title = angular.element(event.target).scope().title;
-    $(event.target).qrcode({text:'https://api.titledb.com/v0/proxy/' + title.titleid});
+    $(event.target).qrcode({text:'https://line-r.github.io/HDBSource/titledb/admin/index.html' + title.titleid});
   });
 
   $('body').on("hidden.bs.collapse", ".qr-collapse", function(event){
