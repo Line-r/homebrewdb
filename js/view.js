858c1ff2d9bdff2a1a892567cfdc808401bd9d94
Line-r <bussens1231@gmail.com>
2019-02-06 19:48:16 +0100

    QR

--- js/view.js
+++ js/view.js
@@ -1,7 +1,7 @@
 $('document').ready(function(){
   $('body').on("show.bs.collapse", ".qr-collapse", function(event){
     var title = angular.element(event.target).scope().title;
-    $(event.target).qrcode({text:'https://line-r.github.io/HDBSource/titledb/admin/index.html' + title.titleid});
+    $(event.target).qrcode({text:'https://raw.githubusercontent.com/Line-r/homebrewdb/master/v0.html' + title.titleid});
   });
 
   $('body').on("hidden.bs.collapse", ".qr-collapse", function(event){
