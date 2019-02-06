2a961c24cc9a2a76c0d1910f1a60ab090659db68
Line-r <bussens1231@gmail.com>
2019-02-07 00:01:17 +0100

    Rollback?

--- js/view.js
+++ js/view.js
@@ -1,7 +1,7 @@
 $('document').ready(function(){
   $('body').on("show.bs.collapse", ".qr-collapse", function(event){
     var title = angular.element(event.target).scope().title;
-    $(event.target).qrcode({text:'https://raw.githubusercontent.com/Line-r/homebrewdb/master/v0.html' + title.name});
+    $(event.target).qrcode({text:'https://raw.githubusercontent.com/Line-r/homebrewdb/master/v0.html' + title.titleid});
   });
 
   $('body').on("hidden.bs.collapse", ".qr-collapse", function(event){
